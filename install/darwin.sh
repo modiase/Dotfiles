ce775f62264f7c3168d8340c703a68ce5f50ce4a
Moye <moyeodiase@hotmail.co.uk>
2022-05-17 16:18:48 +0100

    Added install dir.

---
+++ install/darwin.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# 1. Check for brew, install if not installed 
+
+# 2. Install developer tools
+brew install fd
+brew install fzf
+
+# 3. Install tmux and tpm
+
+# 4. Install alacritty
+
+# 5. Install fish
