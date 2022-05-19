67d8b1390900d9e72dd55aa021ca9b884582ff7e
Moye Odiase <moyeodiase@hotmail.co.uk>
2022-05-19 17:49:18 +0100

    Added fish binding. Added nord theme for alacritty

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,30 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
 source $HOME/Dotfiles/fish/modules/cargo.fish
 
 test -f $HOME/Dotfiles/fish/config.local.fish && source $HOME/Dotfiles/fish/config.local.fish
