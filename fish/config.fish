01c1cdabaf4e634caabdffc905a78f58362ba375
Moye Odiase <moyeodiase@hotmail.co.uk>
2022-05-27 23:48:55 +0100

    Updated fish functions.

--- fish/config.fish
+++ fish/config.fish
@@ -3,24 +3,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-function bind_bang
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t $history[1]; commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
 
-function bind_dollar
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
 
 function fish_user_key_bindings
     bind ! bind_bang
---
+++ fish/functions/clock.fish
@@ -0,0 +1,4 @@
+
+function clock
+	tty-clock -c -C 4 -B
+end
