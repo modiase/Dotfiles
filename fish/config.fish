30805b41d25d60ca6a29afc7d75aa970d2a0c8d6
Moye <moyeodiase@hotmail.co.uk>
2022-08-14 11:41:26 +0100

    Updated fish config to be less breaking on new machines.

--- fish/config.fish
+++ fish/config.fish
@@ -8,14 +8,10 @@ function fish_user_key_bindings
     bind '$' bind_dollar
 end
 
-source $HOME/Dotfiles/fish/modules/cargo.fish
+
+
+set -gx TERM "alacritty"
+set -gx DOTFILES "$HOME/Dotfiles"
 
 test -f $HOME/Dotfiles/fish/config.local.fish && source $HOME/Dotfiles/fish/config.local.fish
 
-set -gx FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden -g "!{**/node_modules/*,**/.git/*}"'
-set -gx TERM "alacritty"
-
-# pnpm
-set -gx PNPM_HOME "/Users/moye/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
