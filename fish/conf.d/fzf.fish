0c4ea275930b165d1e0710038eec0cd0c5f4096b
Moye Odiase <moyeodiase@hotmail.co.uk>
2022-08-17 11:02:22 +0100

    Really fixed fzf for fish this time.

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
 set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --extended $FZF_THEME"
-set -gx FZF_DEFAULT_COMMAND "rg --files --follow --no-ignore-vcs --hidden -g "!{**/node_modules/*,**/.git/*}" fd --type file --hidden --follow --exclude .git"
+set -gx FZF_DEFAULT_COMMAND "rg --files --follow --no-ignore-vcs --hidden -g \"!{**/node_modules/*,**/.git/*}\" 2>/dev/null"
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set -gx FZF_ALT_C_COMMAND "fd --type directory --hidden"
