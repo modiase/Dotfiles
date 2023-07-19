ffaa31b9f5e515d3af7cfc314580dcac883aee6f
Moye Odiase <moyeodiase@hotmail.co.uk>
2023-07-19 12:39:04 +0100

    Added change directory function and binding to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,35 @@
+function change_directory
+    if test -d .git
+        set -f _is_git_repo true
+    else
+        begin
+          set -l info (command git rev-parse --git-dir --is-bare-repository 2>/dev/null)
+          if set -q info[2]; and test $info[2] = false
+              set -f _is_git_repo true
+          else
+              set -f _is_git_repo false
+          end
+        end
+    end
+    if test $_is_git_repo = true
+      set -f root (git rev-parse --show-toplevel)
+    else
+      set -f root (pwd)
+    end
+    cd (cat (echo $root | psub) (fd . --type d $root | psub) | fzf; or echo '.')
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    fish_user_key_bindings
+    bind --mode default \cs change_directory
+    set -gx fish_greeting ""
+    source $HOME/Dotfiles/git/aliases
 end
 
-fish_user_key_bindings
 
 set -gx DOTFILES "$HOME/Dotfiles"
-set -U fish_greeting ""
-
-
 test -f "$HOME/.nix-profile/etc/profile.d/nix.sh";\
     and bass "source $HOME/.nix-profile/etc/profile.d/nix.sh";
 
@@ -17,6 +38,5 @@ test -f "$HOME/.nix-profile/etc/profile.d/nix.sh";\
     or test -f '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh';\
     and bass 'source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
-source $HOME/Dotfiles/git/aliases
 test -f $HOME/.config/fish/config.local.fish; and source $HOME/.config/fish/config.local.fish
 
