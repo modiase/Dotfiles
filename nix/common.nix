47bdc02c424273d268054ffac38362a8adb6e949
Moye <moyeodiase@hotmail.co.uk>
2023-09-29 21:38:28 +0100

    added tsnode

--- nix/common.nix
+++ nix/common.nix
@@ -1,2 +1,2 @@
 with import <nixpkgs> {};
-[ direnv docker fd fzf gcc gtop httpie jq neovim nodejs poetry pstree ripgrep tldr tmux nodePackages.pnpm ]
+[ direnv docker fd fzf gcc gtop httpie jq neovim nodejs poetry pstree ripgrep tldr tmux nodePackages.pnpm nodePackages.ts-node ]
