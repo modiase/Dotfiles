facbf6759c4f48205428d708911a8a35c7faa583
Moye <moyeodiase@hotmail.co.uk>
2023-10-07 12:10:59 +0100

    nix: added typescript as a dependency

--- nix/common.nix
+++ nix/common.nix
@@ -1,2 +1,2 @@
 with import <nixpkgs> {};
-[ direnv docker fd fzf gcc gtop httpie jq neovim nodejs poetry pstree ripgrep tldr tmux nodePackages.pnpm nodePackages.ts-node ]
+[ direnv docker fd fzf gcc gtop httpie jq neovim nodejs poetry pstree ripgrep tldr tmux nodePackages.pnpm nodePackages.ts-node nodePackages.typescript ]
