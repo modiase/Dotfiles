f82e40e8826d09f7260364d3e66ad250de177578
Moye <moyeodiase@hotmail.co.uk>
2023-11-15 22:41:01 +0000

    Added fish right prompt

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,7 @@
+function fish_right_prompt -d "Write out the right prompt"
+    set -l color_dim    (set_color -o 2c2c2c)
+    set -l color_off    (set_color -o normal)
+    set -l timestamp (date '+%H:%M:%S %d/%m/%y')
+
+    echo -n -s $color_dim $timestamp $color_off
+end
