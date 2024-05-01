524bf634956a423e1ac01b4f64f4864f2e6d197b
Moye <moye@FuseEnergy-MacBook-Pro-M3.local>
2024-05-01 10:53:38 +0100

    Add telescope bindings using lua

---
+++ nvim/plugins/telescope.lua
@@ -0,0 +1,4 @@
+-- -- Find files using Telescope command-line sugar.
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', '<cmd>Telescope help_tags<cr>', { noremap = true })
