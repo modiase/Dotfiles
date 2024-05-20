9f3385969a912e274c7d310946a3ffedb76d1c3c
Moye <moye@fuseenergy.com>
2024-05-20 09:42:59 +0100

    Swapped nvim git-conflict jump next/prev bindings

--- nvim/plugins/git-conflict.lua
+++ nvim/plugins/git-conflict.lua
@@ -1,1 +1,7 @@
-require('git-conflict').setup()
+require('git-conflict').setup({ default_mappings = false })
+vim.keymap.set('n', 'co', '<Plug>(git-conflict-ours)')
+vim.keymap.set('n', 'ct', '<Plug>(git-conflict-theirs)')
+vim.keymap.set('n', 'cb', '<Plug>(git-conflict-both)')
+vim.keymap.set('n', 'c0', '<Plug>(git-conflict-none)')
+vim.keymap.set('n', '[x', '<Plug>(git-conflict-prev-conflict)')
+vim.keymap.set('n', ']x', '<Plug>(git-conflict-next-conflict)')
