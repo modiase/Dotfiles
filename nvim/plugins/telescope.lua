05f5d8a5f5108ab392ec575e0ae297d268a88deb
Moye <moye@fuseenergy.com>
2024-05-23 13:00:41 +0100

    Change default telescope word_under_cursor postfix

--- nvim/plugins/telescope.lua
+++ nvim/plugins/telescope.lua
@@ -4,4 +4,5 @@ vim.api.nvim_set_keymap("n", "<leader>fg", ":lua require('telescope').extensions
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ft', '<cmd>Telescope help_tags<cr>', { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fw",
-  ":lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<CR>", { noremap = true })
+  ":lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor({ postfix = \"\"})<CR>",
+  { noremap = true })
