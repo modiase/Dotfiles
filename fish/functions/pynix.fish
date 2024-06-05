96165b1f476c7d2e6beec2b5cb921bb1fc149315
Moye <moye@fuseenergy.com>
2024-06-05 23:00:29 +0100

    Add pynix script to create python nix-shells

---
+++ fish/functions/pynix.fish
@@ -0,0 +1,19 @@
+function pynix -d "Create a python environment with the specified packages using nix."
+    argparse --name=pynix 'h/help' 'v/version=' -- $argv
+    if set -q _flag_h
+        echo "Help"
+        return 0
+    end
+
+    set -l py_version "311"
+    if set -q _flag_v
+        set py_version (echo $_flag_v | awk -F '.' '{ print $1$2 }')
+    end
+
+    set -l nix_command "nix-shell -p python"$py_version
+    for package in $argv
+        set nix_command $nix_command -p python$py_version"Packages."$package
+    end
+    echo "$nix_command"
+    eval "$nix_command"
+end
