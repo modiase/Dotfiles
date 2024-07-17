f3fc26bd089350a274e4089e052ea00922b6ab78
Moye <moye@fuseenergy.com>
2024-07-17 12:01:27 +0100

    fish: add -v flag to envsource
    - Add -v or --verbose flag to envsource.
    - Only print sourced environment variables when -v is passed.
    - Print a usgage message if -h is passed or there is no file argument.

--- fish/functions/envsource.fish
+++ fish/functions/envsource.fish
@@ -1,7 +1,16 @@
-function envsource
+function envsource --description "Supports setting the environment for a fish shell using .env file 'export KEY=VALUE' syntax"
+  argparse --name=envsource 'h/help' 'v/verbose' -- $argv
+  if set -q _flag_h; or not set -q argv[1]
+    echo "usage: envsource <file>" >&2
+    return 1
+  end
+
+
   for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
     set item (string split -m 1 '=' $line)
     set -gx $item[1] $item[2]
-    echo "Exported key $item[1]"
+    if set -q _flag_v
+      echo "export $item[1]=$item[2]"
+    end
   end
 end
