af51c937074e86c623ce07153879374f2ad5c835
Moye <moye@fuseenergy.com>
2024-10-13 20:33:56 +0100

    Add ruff to pynix created environments

--- fish/functions/pynix.fish
+++ fish/functions/pynix.fish
@@ -10,7 +10,7 @@ function pynix -d "Create a python environment with the specified packages using
         set py_version (echo $_flag_v | awk -F '.' '{ print $1$2 }')
     end
 
-    set -l nix_command "nix-shell -p python"$py_version
+    set -l nix_command "nix-shell -p ruff -p python"$py_version
     for package in $argv
         set nix_command $nix_command -p python$py_version"Packages."$package
     end
