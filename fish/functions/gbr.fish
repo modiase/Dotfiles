628854d3cb7b85f6e99073b4772a93f8939a67d3
Moye <moye@fuseenergy.com>
2025-03-18 10:46:18 +0000

    Update gbr -d
    Try force automatically delete if normal delete fails.

--- fish/functions/gbr.fish
+++ fish/functions/gbr.fish
@@ -7,6 +7,12 @@ function gbr
                 read -l -P "Delete branch '$target' [y/N]: " confirm
                 if [ "$confirm" = 'y' ]
                     git branch -d "$target"
+                    if test $status -ne 0
+                        read -l -P "Delete failed. Force delete branch '$target' [y/N]: " force_confirm
+                        if [ "$force_confirm" = 'y' ]
+                            git branch -D "$target"
+                        end
+                    end
                 end
             else
                 git checkout $target
