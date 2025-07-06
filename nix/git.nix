4b714af754aacf4a1174b0d9e24a132d842182e1
Moye <moyeodiase@gmail.com>
2025-07-06 11:46:35 +0100

    git: update vault path prefix

--- nix/git.nix
+++ nix/git.nix
@@ -7,9 +7,8 @@
     userEmail = "moyeodiase@gmail.com";
     extraConfig = {
       core.editor = "nvim";
-      credential.helper = "vault";
+      credential.helper = "vault --vault-path-prefix secret/data/git";
       credential.vault.addr = "http://127.0.0.1:8200";
-      credential.vault.path = "secret/data/git";
       filter.lfs.required = true;
       commit.verbose = true;
       rerere.enabled = true;
