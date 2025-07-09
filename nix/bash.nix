29c4ab4faac01d716286b1161cccbe7a616f94a9
Moye <moyeodiase@hotmail.co.uk>
2025-07-09 09:31:34 +0100

    Fix profile vs rc use

--- nix/bash.nix
+++ nix/bash.nix
@@ -3,15 +3,17 @@
 {
   programs.bash = {
     enable = true;
-    profileExtra = if !pkgs.stdenv.isDarwin then ''
+    initExtra = ''
       if [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
       fi
-    '' else "";
-    initExtra = if !pkgs.stdenv.isDarwin then ''
+      if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+      fi
+    '' + (if !pkgs.stdenv.isDarwin then ''
       if [[ $- == *i* && -z "$IN_NIX_SHELL" && -z "$NO_FISH" ]] && type fish > /dev/null 2>&1; then
         exec fish
       fi
-    '' else "";
+    '' else "");
   };
 }
--- nix/zsh.nix
+++ nix/zsh.nix
@@ -3,15 +3,17 @@
 {
   programs.zsh = {
     enable = true;
-    profileExtra = if pkgs.stdenv.isDarwin then ''
-      if [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
-        . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    initContent = ''
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
       fi
-    '' else "";
-    initContent = if pkgs.stdenv.isDarwin then ''
+      if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+      fi
+    '' + (if pkgs.stdenv.isDarwin then ''
       if [[ $- == *i* && -z "$IN_NIX_SHELL" && -z "$NO_FISH" ]] && type fish > /dev/null 2>&1; then
         exec fish
       fi
-    '' else "";
+    '' else "");
   };
 }
