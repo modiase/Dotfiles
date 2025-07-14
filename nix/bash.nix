9a6436d3b019a6694363f2500ec455211f2afadf
Moye Odiase <moyeodiase@gmail.com>
2025-07-14 15:15:06 +0100

    More robust check for nix env

--- nix/bash.nix
+++ nix/bash.nix
@@ -15,10 +15,23 @@
       + (
         if !pkgs.stdenv.isDarwin then
           ''
+            in_nix_process() {
+                local pid=$PPID
+                while [ $pid -ne 1 ]; do
+                    local comm=$(ps -o comm= -p $pid 2>/dev/null)
+                    if [[ "$comm" == *nix* ]] || [[ "$comm" == *"nix-daemon"* ]]; then
+                        return 0
+                    fi
+                    pid=$(ps -o ppid= -p $pid 2>/dev/null | tr -d ' ')
+                    [ -z "$pid" ] && break
+                done
+                return 1
+            }
+
             if [[ $- == *i* && -z "$NO_FISH" && -z "$IN_NIX_SHELL" && -z "$NIX_BUILD_CORES" && -z "$__structuredAttrs" ]] && \
                type fish > /dev/null 2>&1 && \
-               ! ps -o comm= -p $PPID 2>/dev/null | grep -q "nix"; then
-              exec fish
+               ! in_nix_process; then
+               exec fish
             fi
           ''
         else
