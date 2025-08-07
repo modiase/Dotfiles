68458e1c622d8414906e6878046cfa9cc54a31a2
Moye Odiase <moyeodiase@hotmail.co.uk>
2025-08-07 09:26:18 +0100

    Remove double wrapping
    These functions don't execute the first time because home-manager wraps
    the implementation file in a function block.

--- fish/functions/gbr.fish
+++ fish/functions/gbr.fish
@@ -1,26 +1,22 @@
-function gbr
-    argparse d/delete -- $argv
-    if test -z "$argv"
-        set target (git branch | fzf | sed -e 's/^ *[\*\+]* *//')
-        if test -n "$target"; and [ "$target" != "$(git branch --show)" ]
-            if test -n "$_flag_d"
-                read -l -P "Delete branch '$target' [y/N]: " confirm
-                if [ "$confirm" = y ]
-                    git branch -d "$target"
-                    if test $status -ne 0
-                        read -l -P "Delete failed. Force delete branch '$target' [y/N]: " force_confirm
-                        if [ "$force_confirm" = y ]
-                            git branch -D "$target"
-                        end
+argparse d/delete -- $argv
+if test -z "$argv"
+    set target (git branch | fzf | sed -e 's/^ *[\*\+]* *//')
+    if test -n "$target"; and [ "$target" != "$(git branch --show)" ]
+        if test -n "$_flag_d"
+            read -l -P "Delete branch '$target' [y/N]: " confirm
+            if [ "$confirm" = y ]
+                git branch -d "$target"
+                if test $status -ne 0
+                    read -l -P "Delete failed. Force delete branch '$target' [y/N]: " force_confirm
+                    if [ "$force_confirm" = y ]
+                        git branch -D "$target"
                     end
                 end
-            else
-                git checkout $target
             end
+        else
+            git checkout $target
         end
-    else
-        git branch $_flag_d $argv
     end
+else
+    git branch $_flag_d $argv
 end
-
-complete -c gbr -w 'git branch'
