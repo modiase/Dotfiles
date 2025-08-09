4aa71c6f10f7a6e7f00f1a7f8e3f68635c59461d
Moye Odiase <moyeodiase@hotmail.co.uk>
2025-08-09 09:42:48 +0100

    Inline common.nix
    Simplify file structure

--- nix/home.nix
+++ nix/home.nix
@@ -28,7 +28,59 @@
 
   home.username = "moye";
 
-  home.packages = with pkgs; ((import ./common.nix { inherit pkgs; }));
+  home.packages = with pkgs; [
+    awscli2
+    cargo
+    claude-code
+    codex-cli
+    coreutils
+    direnv
+    docker
+    fd
+    fzf
+    go
+    gopls
+    google-cloud-sdk
+    gcc
+    gemini-cli
+    gnused
+    gpt-cli
+    httpie
+    jq
+    jwt-cli
+    kubectl
+    ngrok
+    nix-prefetch-git
+    nix-tree
+    nixfmt-rfc-style
+    nodePackages.pnpm
+    nodePackages.ts-node
+    nodePackages.typescript
+    nodejs
+    pass
+    pass-git-helper
+    poetry
+    pre-commit
+    pstree
+    (python313.withPackages (
+      ps: with ps; [
+        boto3
+        ipython
+        matplotlib
+        numpy
+        pandas
+        ruff
+      ]
+    ))
+    pstree
+    ripgrep
+    tldr
+    tree
+    tshark
+    uv
+    watch
+    wireguard-tools
+  ];
 
   home.file.".config/nvim" = {
     source = ../nvim;
