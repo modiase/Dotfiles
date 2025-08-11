6fbabf6560cb32591644ac3fb8b9964dc2f535f8
Moye Odiase <moyeodiase@hotmail.co.uk>
2025-08-11 13:32:02 +0100

    Fetch from origin before grbom

--- fish/functions/grbom.fish
+++ fish/functions/grbom.fish
@@ -3,4 +3,5 @@ if test $status -ne 0
     echo "Failed to determine default branch" >&2
     return 1
 end
+gfch origin $default_branch:$default_branch &>/dev/null; and echo "Updated $default_branch from origin"; or echo 'Warning: could not fetch updates from origin. Is it defined?'
 git rebase $default_branch $argv
