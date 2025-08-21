9c06aab46434abfe7d56caae85dcf59879b29d08
Moye Odiase <moyeodiase@hotmail.co.uk>
2025-08-21 14:46:29 +0100

    Disable git rerere
    It makes unhelpful assumptions.

--- nix/git.nix
+++ nix/git.nix
@@ -11,7 +11,6 @@
         if pkgs.stdenv.isDarwin then "osxkeychain" else "${pkgs.pass-git-helper}/bin/pass-git-helper";
       filter.lfs.required = true;
       commit.verbose = true;
-      rerere.enabled = true;
       gpg.format = "openpgp";
       gpg.openpgp.program = "${pkgs.gnupg}/bin/gpg";
       push.autoSetupRemote = true;
