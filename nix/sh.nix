9fbee32b28d2b3eb05afe5d097fa95aa0854b965
Moye Odiase <moyeodiase@gmail.com>
2025-09-19 16:36:03 +0100

    Launch fish on alacritty start

--- nix/sh.nix
+++ nix/sh.nix
@@ -23,7 +23,7 @@ let
        ! in_nix_environment && \
        [[ -z "$CURSOR_AGENT" ]] && \
        [[ -t 0 && -t 1 ]] && \
-       [[ "$(ps -o comm= -p $PPID)" =~ (login|sshd|tmux|screen|zsh|bash) ]]; then
+       [[ "$(ps -o comm= -p $PPID)" =~ (login|sshd|tmux|screen|zsh|bash|alacritty) ]]; then
        exec fish
     fi
   '';
