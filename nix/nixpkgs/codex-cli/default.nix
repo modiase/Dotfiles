eaa03e3f7fa8242a54379d78360c32db4478b4e6
Moye <moyeodiase@hotmail.co.uk>
2025-09-24 14:59:27 +0100

    Bump flake and codex

--- nix/nixpkgs/codex-cli/default.nix
+++ nix/nixpkgs/codex-cli/default.nix
@@ -10,19 +10,19 @@ let
   platformInfo = {
     "x86_64-linux" = {
       target = "x86_64-unknown-linux-gnu";
-      sha256 = "sha256-N7qOby5yq9VZzOs0DWMZbWKLU7s2P2vs3uLwDhIeFRw=";
+      sha256 = "sha256-uz7BQsbBO0r3GrJaLmiz1tCE5HRT55QYnBlvTcmrqss=";
     };
     "aarch64-linux" = {
       target = "aarch64-unknown-linux-gnu";
-      sha256 = "sha256-PLACEHOLDER-FOR-AARCH64-LINUX";
+      sha256 = "sha256-YJqnjiZALQ7DiI7PgABO8qb61zQ2JLApmE4r07SVaZQ=";
     };
     "x86_64-darwin" = {
       target = "x86_64-apple-darwin";
-      sha256 = "sha256-PLACEHOLDER-FOR-X86_64-DARWIN";
+      sha256 = "sha256-BKvqqqL5KRKQfQ5/Fh/neDv+8VvsxFgRRTVNyIOAft8=";
     };
     "aarch64-darwin" = {
       target = "aarch64-apple-darwin";
-      sha256 = "sha256-4abbcbPTZx9GOpM3eEBUOU8TDtpB2G+ttyCR+w+J38E=";
+      sha256 = "sha256-yZitmJdMATf6CfCUI78/Uvn6X853MfSJVr0I3AENJVc=";
     };
   };
 
@@ -33,7 +33,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "codex-cli";
-  version = "0.39.0";
+  version = "0.40.0";
 
   src = fetchurl {
     url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-${currentPlatform.target}.tar.gz";
