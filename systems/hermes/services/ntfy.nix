d57ba345a9c353406c61d63a96ea98e39b7ae9d0
Moye Odiase <moyeodiase@gmail.com>
2025-10-03 22:10:28 +0100

    Fix ntfy configuration

--- systems/hermes/services/ntfy.nix
+++ systems/hermes/services/ntfy.nix
@@ -8,15 +8,16 @@
       listen-http = ":8080";
       cache-file = "/var/lib/ntfy/cache.db";
       attachment-cache-dir = "/var/lib/ntfy/attachments";
-      auth-file = "/var/lib/ntfy/user.db";
-      auth-default-access = "deny-all";
       behind-proxy = true;
       upstream-base-url = "https://ntfy.sh";
     };
   };
 
-  systemd.tmpfiles.rules = [
-    "d /var/lib/ntfy 0750 ntfy-sh ntfy-sh -"
-    "d /var/lib/ntfy/attachments 0750 ntfy-sh ntfy-sh -"
-  ];
+  systemd.services.ntfy-sh = {
+    preStart = ''
+      mkdir -p /var/lib/ntfy/attachments
+      chown -R ntfy-sh:ntfy-sh /var/lib/ntfy
+      chmod 755 /var/lib/ntfy
+    '';
+  };
 }
