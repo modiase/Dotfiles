29ba0ef4dc6c23927f3bb80927bdcf220e217551
Moye <moyeodiase@hotmail.co.uk>
2025-10-28 13:53:12 +0000

    Bump codex version

--- nix/nixpkgs/codex-cli/default.nix
+++ nix/nixpkgs/codex-cli/default.nix
@@ -7,22 +7,25 @@
 }:
 
 let
+  repo = "openai/codex";
+  releasesBase = "https://github.com/${repo}/releases/download";
+
   platformInfo = {
     "x86_64-linux" = {
       target = "x86_64-unknown-linux-gnu";
-      sha256 = "sha256-uz7BQsbBO0r3GrJaLmiz1tCE5HRT55QYnBlvTcmrqss=";
+      sha256 = "sha256-QkAjKdvc6wRDFqghcPaFMbr8/0Ps7D2IUEGLLKZpMoc=";
     };
     "aarch64-linux" = {
       target = "aarch64-unknown-linux-gnu";
-      sha256 = "sha256-YJqnjiZALQ7DiI7PgABO8qb61zQ2JLApmE4r07SVaZQ=";
+      sha256 = "sha256-FEYZc6OYs2l9yzVo+FdE/lbeAUtAojo/B5Fs//YiQQY=";
     };
     "x86_64-darwin" = {
       target = "x86_64-apple-darwin";
-      sha256 = "sha256-BKvqqqL5KRKQfQ5/Fh/neDv+8VvsxFgRRTVNyIOAft8=";
+      sha256 = "sha256-irPhihawfuMrlVVw5UbpkNd73783MqxT22putEfw/6c=";
     };
     "aarch64-darwin" = {
       target = "aarch64-apple-darwin";
-      sha256 = "sha256-yZitmJdMATf6CfCUI78/Uvn6X853MfSJVr0I3AENJVc=";
+      sha256 = "sha256-m4xyPkVkdrM5SvPfez3JgmvnavE3OZlY3Ghs1cm2j/4=";
     };
   };
 
@@ -33,10 +36,10 @@ in
 
 stdenv.mkDerivation rec {
   pname = "codex-cli";
-  version = "0.40.0";
+  version = "0.50.0";
 
   src = fetchurl {
-    url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-${currentPlatform.target}.tar.gz";
+    url = "${releasesBase}/rust-v${version}/codex-${currentPlatform.target}.tar.gz";
     sha256 = currentPlatform.sha256;
   };
 
@@ -64,7 +67,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Lightweight coding agent that runs in your terminal";
-    homepage = "https://github.com/openai/codex";
+    homepage = "https://github.com/${repo}";
     license = licenses.asl20;
     maintainers = [ ];
     platforms = platforms.unix;
